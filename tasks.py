7a6adb77e884c9c2bae0b212f9dec97c8fefaad2
WH1NER7 <danil-2208@mail.ru>
2024-08-13 02:52:19 +0500

    upd add user to extras

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 # tasks.py
-from associated_advertisement_goods import collect_data
+# from associated_advertisement_goods import collect_data
 from celery_config import app
 
 
@@ -8,7 +8,7 @@ def add(x, y):
     return x + y
 
 
-@app.task
-def collect_data_task(company_api_key, api_key, start_date, end_date, authorizev3, cookie, user_agent):
-    print('111')
-    return collect_data(company_api_key, api_key, start_date, end_date, authorizev3, cookie, user_agent)
+# @app.task
+# def collect_data_task(company_api_key, api_key, start_date, end_date, authorizev3, cookie, user_agent):
+#     print('111')
+#     return collect_data(company_api_key, api_key, start_date, end_date, authorizev3, cookie, user_agent)
