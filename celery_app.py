d31852f7d72490fff6c05d84c658c0777285101f
WH1NER7 <danil-2208@mail.ru>
2024-08-13 03:10:37 +0500

    upd add user to extras

--- celery_app.py
+++ celery_app.py
@@ -2,6 +2,6 @@ from celery import Celery
 
 celery_app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
 
-@celery_app.task
+@celery_app.task(name='celery_app.async_task_test')
 def async_task_test(x, y):
     return x + y
