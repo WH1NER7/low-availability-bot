971fc85f117c32ee4b6f0eedca089695d605687f
WH1NER7 <danil-2208@mail.ru>
2024-08-14 02:28:04 +0500

    upd add user to extras

--- celery_app.py
+++ celery_app.py
@@ -3,9 +3,10 @@ from associated_advertisement_goods import collect_data
 from aiogram import Bot
 from aiogram.types import InputFile
 import os
+import asyncio
 
 celery_app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
-bot = Bot(token=os.getenv('BOT_TOKEN'))
+# bot = Bot(token=os.getenv('BOT_TOKEN'))
 
 
 @celery_app.task(name='celery_app.async_task_test')
@@ -18,15 +19,16 @@ def collect_data_task(company_api_key, api_key, start_date, end_date, authorizev
     return collect_data(company_api_key, api_key, start_date, end_date, authorizev3, cookie, user_agent)
 
 
-
 @celery_app.task(name='celery_app.on_task_complete')
 def on_task_complete(result, chat_id):
-    print(f"Result: {result}, Chat ID: {chat_id}")  # Добавьте это для отладки
+    async def send_document():
+        bot = Bot(token=os.getenv('BOT_TOKEN'))
+        await bot.send_document(chat_id, InputFile(result))
 
     if not os.path.exists(result):
-        raise FileNotFoundError(f"Result: {result}, Chat ID: {chat_id}")
+        raise FileNotFoundError(f"Файл не найден: {result}")
 
-    bot.send_document(chat_id, InputFile(result))
+    asyncio.run(send_document())
 
 
 
